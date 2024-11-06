05393fdd7f7893f47886d9981e7f1fcd728e32ac
Daniel Benson <danielnbenson@gmail.com>
2024-11-06 17:12:12 +0100

    commiti

--- InvoApp.Models/DtoModels/UserInfoDTO.cs
+++ InvoApp.Models/DtoModels/UserInfoDTO.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace InvoApp.Models.DtoModels
 {
-    internal class UserInfoDTO
+    public class UserInfoDTO
     {
+        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
     }
 }
